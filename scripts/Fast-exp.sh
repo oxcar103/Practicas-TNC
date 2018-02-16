7f1758c1b29793a99a199a8fae082ac0e3f1d78b
oxcar103 <oxcar103@gmail.com>
2018-02-16 13:01:38 +0100

    Comentando el script

--- scripts/Fast-exp.sh
+++ scripts/Fast-exp.sh
@@ -1,25 +1,26 @@
 #! /bin/bash
 
 # Parámetros del programa:
-base=$1             # Primer parámetro: base
-exp=$2              # Segundo parámetro: exponente
-mod=$3              # Tercer parámetro: módulo
+base=$1                                         # Primer parámetro: base
+exp=$2                                          # Segundo parámetro: exponente
+mod=$3                                          # Tercer parámetro: módulo
 
-digits=`echo "obase=2; $exp" | bc | fold -w1`
+digits=`echo "obase=2; $exp" | bc | fold -w1`   # Convertimos el exponente a binario y con los dígitos separados
 acu=1
 c_exp=0
 
-for i in $digits
+for i in $digits                                # Recorremos cada dígito
     do
-        let acu=acu**2%mod
-        let c_exp=2*c_exp
+        let acu=acu**2%mod                      # Elevamos al cuadrado y hacemos módulo
+        let c_exp=2*c_exp                       # Duplicamos el exponente
 
+        # Si el dígito actual es 1...
         if (( $i == 1))
         then
-            let acu=acu*base%mod
-            let c_exp=c_exp+1
+            let acu=acu*base%mod                # Multiplicamos además por la base
+            let c_exp=c_exp+1                   # Sumamos 1 al exponente
         fi
 
-        echo acu = $acu, exp = $c_exp
+        echo acu = $acu, exp = $c_exp           # Devolvemos el valor y exponente actuales
     done
 
