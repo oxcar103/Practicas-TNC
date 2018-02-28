9aa38d73d52d155e15e7310b7c6f09f28b81b78e
oxcar103 <oxcar103@gmail.com>
2018-02-28 23:32:44 +0100

    Script de factorización en primos Tiene la misma función e incluso salida que el comando factors aunque menos eficiente obviamente. Aún así, me hacía ilusión probar si mi algoritmo funcionaba y por eso lo implementé.

---
+++ scripts/Prime_factors.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+# Parámetros del programa:
+n=$1                                            # Primer parámetro: número a descomponer en primos
+lim=$(echo "sqrt($n)/1" | bc)
+prime=2
+
+echo -n "$n:" 
+
+while [ $prime -le $lim ]                       # Iteramos hasta que un valor sea el doble de q_0 porque el perido acaba ahí (hay un teorema)
+    do
+        while (( $n % $prime == 0 ))
+            do
+                echo -n " $prime"
+
+                let n=n/prime
+                lim=$(echo "sqrt($n)/1" | bc)
+            done
+
+        prime=$(primes $prime | head -n 2 | tail -n 1)
+    done
+
+if (( $n != 1))
+        then
+            echo -n " $n"
+        fi
+
+echo ""
