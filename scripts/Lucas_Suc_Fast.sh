b9ffc75d8b9d4cd45c0fe8bb9d66e05fe41dc758
oxcar103 <oxcar103@gmail.com>
2018-03-10 04:02:10 +0100

    Mejora del algoritmo

--- scripts/Lucas_Suc_Fast.sh
+++ scripts/Lucas_Suc_Fast.sh
@@ -3,28 +3,29 @@
 # Parámetros del programa:
 P=$1                                            # Primer parámetro: valor de P
 Q=$2                                            # Segundo parámetro: valor de Q
-n=$3                                            # Tercer parámetro: exponente
+mod=$3                                          # Tercer parámetro: módulo
 
 let Delta=P**2-4*Q                              # Calculamos Delta, el valor del interior de la raíz
-let half=(n+1)/2%n                              # Calculamos el inverso de 2 módulo n
-let half_P=P*half%n                             # Calculamos P/2 módulo n
+let half=(mod+1)/2%mod                          # Calculamos el inverso de 2 módulo n
+let half_P=P*half%mod                           # Calculamos P/2 módulo n
+symbol=`./scripts/Jacobi_symbol.sh $Delta $mod` # Calculamos el símbolo de Jacobi
+let exp=mod-symbol                              # Calculamos el exponente
 
-echo $Delta $half $half_P
+echo $half_P, $half, $Delta, $exp, $mod
 
 touch tmp.txt                                   # Archivo para el scipt Fast_exp_irr.sh
-./scripts/Fast_exp_irr.sh $half_P $half $Delta $n $n
+./scripts/Fast_exp_irr.sh $half_P $half $Delta $exp $mod > /dev/null
 
 for j in `cat tmp.txt`
     do
         i=`echo $j | cut -f 3 -d ","`
 
         aux=`echo $j | cut -f 1 -d ","`
-        let aux=aux*2%n
-        echo -n "V_$i = $aux, "
+        let aux=aux*2%mod
+        echo -ne "V_$i = $aux,\t"
         aux=`echo $j | cut -f 2 -d ","`
-        let aux=aux*2%n
+        let aux=aux*2%mod
         echo "U_$i = $aux"
-
     done
 
 rm tmp.txt                                      # Eliminamos el archivo temporal
