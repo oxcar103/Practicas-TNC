316356adb6360413f7b584aa9d81b3d566ba55cd
oxcar103 <oxcar103@gmail.com>
2018-03-23 14:23:34 +0100

    Test de Miller-Rabin

---
+++ scripts/Miller-Rabin_test.sh
@@ -0,0 +1,52 @@
+#! /bin/bash
+
+# Parámetros del programa:
+n=$1                                            # Primer parámetro: número
+p=$2                                            # Segundo parámetro: primo
+
+let m=n-1
+acu=`./scripts/Fast_exp_int.sh $p $m $n | tail -n 1 | cut -f 1 -d , | cut -f 2 -d =`
+
+if [ $acu -ne "1" ]
+then
+    echo "No es primo"
+else
+    i=0
+
+    while (( $m % 2 == 0 ))
+    do
+        let m=m/2
+        let i=i+1
+    done
+
+    acu=`./scripts/Fast_exp_int.sh $p $m $n | tail -n 1 | cut -f 1 -d , | cut -f 2 -d =`
+
+    if [ $acu -eq "1" ] || [ $acu -eq $(($n-1)) ]
+    then
+        echo "Es primo"
+    else
+        while [ $i -gt 0 ]
+        do
+            let acu=acu**2%n
+
+            if [ $acu -eq "1" ]
+            then
+                echo "No es primo"
+                i=0
+            elif [ $acu -eq $(($n-1)) ]
+            then
+                echo "Es primo"
+                i=0
+            else
+                if [ $i -eq "1" ]
+                then
+                    echo "No es primo"
+                fi
+                
+                let i=i-1
+            fi
+
+            echo "i = $i, $acu"
+        done
+    fi
+fi
