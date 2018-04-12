4696e1b8f48781cca7eee087b1a669680ad50438
oxcar103 <oxcar103@gmail.com>
2018-04-12 12:13:54 +0200

    Inversos en módulo

---
+++ scripts/Invert_mod.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+# Parámetros del programa:
+num=$1                                          # Primer parámetro: número que invertir
+p=$2                                            # Segundo parámetro: primo
+
+# Si num es nulo...
+if [ $num -eq "0" ]
+then
+    inv=0                                       # No existe el inverso
+
+# Si no...
+else
+    # Buscamos el inverso a fuerza bruta...
+    for i in `seq $(( p-1 ))`
+    do
+        prod=`echo "($num*$i)%$p" | bc`
+
+        if [ $prod -eq "1" ]                    # Si p es primo, es único
+        then
+            inv=$i                              # Guardamos el inverso
+        fi
+    done
+fi
+
+# Devolvemos el inverso
+echo $inv
