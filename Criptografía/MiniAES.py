795b107ba5a0d836ca996c7764d4f1e8251404a3
oxcar103 <oxcar103@gmail.com>
2018-04-24 19:02:53 +0200

    Tabla de exponentes

---
+++ Criptografía/MiniAES.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+EXP = {
+0x0: 0b0000, 0x1: 0b0010, 0x2: 0b0100, 0x3: 0b1000,
+0x4: 0b0011, 0x5: 0b0110, 0x6: 0b1100, 0x7: 0b1011,
+0x8: 0b0101, 0x9: 0b1010, 0xA: 0b0111, 0xB: 0b1110,
+0xC: 0b1111, 0xD: 0b1101, 0xE: 0b1001, 0xF: 0b0001}
+EXP_INV = {}
+
+for i in EXP:
+  EXP_INV[EXP[i]] = i
+
+
